bfc9c2e6e8e2161ccbe74814086548c4b3e46516
PedroTexugo <pedromigueltexugo@outlook.com>
2017-12-06 19:29:58 +0000

    Duas regressões working

--- proj2alunos/P2/regsol.py
+++ proj2alunos/P2/regsol.py
@@ -1,17 +1,16 @@
 import numpy as np
 from sklearn import datasets, tree, linear_model
 from sklearn.kernel_ridge import KernelRidge
-from sklearn.linear_model import LinearRegression
-from sklearn.neighbors import KNeighborsRegressor
 from sklearn.model_selection import cross_val_score
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.pipeline import Pipeline
-from sklearn.linear_model import HuberRegressor
+from sklearn import svm
+from sklearn.ensemble import GradientBoostingRegressor
 import timeit
 
 def mytraining(X,Y):
-    #reg = KernelRidge(kernel='rbf', gamma=0.05, alpha = 1e-05)
-    reg = Pipeline([('poly', PolynomialFeatures(degree=10)),  ('linear', HuberRegressor(alpha=0.1))])
+    #reg = KernelRidge(kernel='rbf', gamma=0.1, alpha = 0.001)
+    reg = svm.SVR(gamma=0.1, epsilon=0.01, C=1000)
     reg.fit(X,Y)
     return reg
     
